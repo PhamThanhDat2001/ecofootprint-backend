22962deb06c9b99683650aa09954715fa8f9feeb
phamdat <phamthanhdat186@gmail.com>
2023-11-14 10:46:24 +0700

    init project

---
+++ src/main/java/com/doan/ecofootprint_be/EcoFootPrintBeApplication.java
@@ -0,0 +1,13 @@
+package com.doan.ecofootprint_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFootPrintBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoFootPrintBeApplication.class, args);
+	}
+
+}
